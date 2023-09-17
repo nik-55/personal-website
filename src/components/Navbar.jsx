f5ceed3640c97a4770c49fde0fb52067e464ddb1
nik-55 <nik.xyz.in@gmail.com>
2023-09-17 19:41:30 +0530

    Style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 import profile_img from '@/assets/images/profile.jpg';
+import twitter from '@/assets/icons/twitter-x.svg';
+import linkedin from '@/assets/icons/linkedin.svg';
+import github from '@/assets/icons/github.svg';
 import Image from 'next/image';
 
 const Navbar = () => {
@@ -21,12 +24,12 @@ const Navbar = () => {
         </a>
         <div
           className='offcanvas offcanvas-start'
-          style={{ width: '250px' }}
+          style={{ width: 'fit-content' }}
           tabIndex='-1'
           id='offcanvasNavbar'
           aria-labelledby='offcanvasNavbarLabel'
         >
-          <div className='offcanvas-body'>
+          <div className='offcanvas-header border border-bottom d-flex flex-column'>
             <Image
               src={profile_img}
               className='border border-dark rounded-circle'
@@ -35,6 +38,12 @@ const Navbar = () => {
               alt='Nikhil Mahajan'
               id='offcanvasNavbarLabel'
             />
+            <h5 className='offcanvas-title' id='offcanvasRightLabel'>
+              Nikhil Mahajan
+            </h5>
+            <span class='badge bg-secondary'>Software Developer</span>
+          </div>
+          <div className='offcanvas-body'>
             <ul className='navbar-nav mt-2 justify-content-end flex-grow-1 pe-3'>
               <li
                 className='nav-item'
@@ -91,7 +100,34 @@ const Navbar = () => {
                   Blogs
                 </Link>
               </li>
+
+              <li
+                className='nav-item'
+                data-bs-dismiss='offcanvas'
+                aria-label='Close'
+              >
+                <a
+                  className='nav-link'
+                  target={'_blank'}
+                  href='https://github.com/nik-55'
+                >
+                  Resume
+                </a>
+              </li>
             </ul>
+          </div>
+          <div className='offcanvas-header d-flex flex-column p-0 border border-top'>
+            <div className='w-50 d-flex justify-content-around'>
+              <a href='https://google.com'>
+                <Image src={twitter} width={20} height={20} alt='twitter' />
+              </a>
+              <a href='https://google.com'>
+                <Image src={github} width={20} height={20} alt='github' />
+              </a>
+              <a href='https://google.com'>
+                <Image src={linkedin} width={20} height={20} alt='linkedin' />
+              </a>
+            </div>
           </div>
         </div>
       </div>
